e0709e52bf5b6321c5e6a83498cf48e92cf17a70
rmejia224 <richardmejia224@gmail.com>
2026-03-29 21:47:01 -0400

    Temporarily hardcode production API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
-const apiBaseUrl = import.meta.env.VITE_API_URL;
-
-console.log("VITE_API_URL:", apiBaseUrl);
-
 const api = axios.create({
-  baseURL: apiBaseUrl,
+  baseURL: "https://capstone-project-final.onrender.com/api",
 });
 
 export default api;
