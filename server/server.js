55cc4ff5288b194f1a8441be5a782aee95f696ae
rmejia224 <richardmejia224@gmail.com>
2026-03-29 20:47:27 -0400

    changes made

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use("/api/workouts", require("./routes/workoutRoutes"));
 app.use("/api/records", require("./routes/recordRoutes"));
 app.use("/api/progress", require("./routes/progressRoutes"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
